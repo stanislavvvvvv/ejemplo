fe38ec122a9590a9bcf057bcfa3750905d3f7975
stanislav <stas.hiznicenco@gmail.com>
2019-09-03 16:40:07 +0200

    primer commit

--- src/main/java/com/ejemplo/entities/Coche.java
+++ src/main/java/com/ejemplo/entities/Coche.java
@@ -10,6 +10,48 @@ public abstract class Coche {
 
 	public abstract void movimiento();
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((caballos == null) ? 0 : caballos.hashCode());
+		result = prime * result + ((puertas == null) ? 0 : puertas.hashCode());
+		result = prime * result + ((traccion == null) ? 0 : traccion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coche other = (Coche) obj;
+		if (caballos == null) {
+			if (other.caballos != null)
+				return false;
+		} else if (!caballos.equals(other.caballos))
+			return false;
+		if (puertas == null) {
+			if (other.puertas != null)
+				return false;
+		} else if (!puertas.equals(other.puertas))
+			return false;
+		if (traccion == null) {
+			if (other.traccion != null)
+				return false;
+		} else if (!traccion.equals(other.traccion))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Coche [caballos=" + caballos + ", puertas=" + puertas + ", traccion=" + traccion + "]";
+	}
+
 	public Coche() {
 
 	}
--- src/test/java/com/ejemplo/AppTest.java
+++ src/test/java/com/ejemplo/AppTest.java
@@ -1,7 +1,12 @@
 package com.ejemplo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
 import com.ejemplo.entities.Coche;
 import com.ejemplo.entities.Economico;
+import com.ejemplo.entities.Todoterreno;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -40,6 +45,26 @@ public class AppTest
     }
     //comparar dos coche con los mismo parametros assertj
     //lista de coche a ver si son iguales assertj
+    public void testLista() {
+    	ArrayList<Coche> coches = new ArrayList<Coche>();
+    	ArrayList<Coche> coches2 = new ArrayList<Coche>();
+    	Coche coche = new Economico();
+    	coche.setCaballos(150);
+    	coche.setPuertas(3);
+    	coche.setTraccion("delantera");
+    	Coche coch2e = new Todoterreno();
+    	coch2e.setCaballos(150);
+    	coch2e.setPuertas(3);
+    	coch2e.setTraccion("cuatroxcuatro");
+    	
+    	coches.add(coche);
+    	coches.add(coch2e);
+    	coches2.add(coche);
+    	coches2.add(coch2e);
+    	assertThat(coches).isEqualTo(coches2);
+    }
+    
+    
     public void testCoche() {
     	Coche coche = new Economico();
     	coche.setCaballos(150);
